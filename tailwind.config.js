dcc61e8218aa3804a0e115470e89a0465c7a3986
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-04-17 15:00:25 +0600

    4 pages done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'], // Replace 'Inter' with the name of the Google Font you selected
+      },
+    },
+  },
+  plugins: [],
+}
+
